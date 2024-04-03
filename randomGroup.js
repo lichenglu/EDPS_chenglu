92001b767e992510d514031703a4696839bdd07d
Chenglu Li <kabelee92@gmail.com>
2024-04-03 17:01:50 -0600

    Co-authored-by: W. Jordan Charles <williamthe5thc@users.noreply.github.com> Co-authored-by: EDPS6560CamdenRobinson <EDPS6560CamdenRobinson@users.noreply.github.com>

--- randomGroup.js
+++ randomGroup.js
@@ -1,13 +1,13 @@
 const students = ['camden', 'genevieve', 'laura', 'natalia', 'jiwon', 'jordan']
 
-function formPairGroups(roster) {
+function formPairGroups(roster, peoplePerGroup = 3) {
     roster = [...roster]
     let tempGroup = []
     const groups = []
     while (roster.length > 0) {
         const idx = Math.floor(Math.random() * roster.length)
         tempGroup.push(roster.splice(idx, 1)[0])
-        if (tempGroup.length == 2 || roster.length == 0) {
+        if (tempGroup.length == peoplePerGroup || roster.length == 0) {
             groups.push(tempGroup)
             tempGroup = []
         }
